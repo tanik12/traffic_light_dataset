5720ec3f0e5b8ad525d7e7d5a009df70de60973e
tanik12 <koyu.tani01@g.softbank.co.jp>
2020-01-16 19:52:44 +0900

    [feat]フォルダ名を指定できるようにした。

--- rename.py
+++ rename.py
@@ -1,20 +1,26 @@
 import glob
 import os
+import sys
  
-# 拡張子.txtのファイルを取得する
-img_path = './traffic_light/*.jpg'
-i = 1
- 
-# txtファイルを取得する
-file_lists = glob.glob(img_path)
-print('変更前')
-print(file_lists)
- 
-# ファイル名を一括で変更する
-for file_name in file_lists:
-  os.rename(file_name, './traffic_light/traffic_' + "%06.f.jpg" % i)
-  i += 1
- 
-li = glob.glob(img_path)
-print('変更後')
-print(li)
+def main():
+    # 拡張子.jpgのファイルを取得する
+    args = sys.argv
+    img_path = './' + args[1] + '/*.jpg'
+    i = 1
+     
+    # image pathを取得する
+    file_lists = glob.glob(img_path)
+    print('変更前')
+    print(file_lists)
+     
+    # image nameを一括で変更する
+    for file_name in file_lists:
+      os.rename(file_name, './' + args[1] + '/traffic_' + "%06.f.jpg" % i)
+      i += 1
+     
+    li = glob.glob(img_path)
+    print('変更後')
+    print(li)
+
+if __name__ == "__main__":
+    main()
